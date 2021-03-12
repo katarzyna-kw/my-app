985c1b3e1dba7004ad87c4152c7f7001fe91614d
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-11 20:51:06 -0600

    add form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,26 @@ export default class App extends Component {
     return (
       <Container fluid className="container">
         <Header as='h2'>React Google Sheets!</Header>
+        <Form className="form">
+          <Form.Field>
+            <label>Name</label>
+            <input placeholder='Enter your name' />
+          </Form.Field>
+          <Form.Field>
+            <label>Age</label>
+            <input placeholder='Enter your age' />
+          </Form.Field>
+          <Form.Field>
+            <label>Salary</label>
+            <input placeholder='Enter your salary' />
+          </Form.Field>
+          <Form.Field>
+            <label>Hobby</label>
+            <input placeholder='Enter your hobby' />
+          </Form.Field>
+          
+          <Button color="blue" type='submit'>Submit</Button>
+        </Form>
       </Container>
     );
   }

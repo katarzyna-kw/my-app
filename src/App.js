b0fb295cc30608e490266230c5ec055e6267ec90
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-18 09:05:48 -0500

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,85 +1,70 @@
-import React, { useState, Component } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { Button, Form, Header } from 'semantic-ui-react'
 import './App.css';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       beyonce: '',
-       whitney: '',
-       britney: '',
-       mariah: ''
-    }
-  }
+function App() {
+  const [beyonce, setBeyonce] = useState('');
+  const [whitney, setWhitney] = useState('');
+  const [britney, setBritney] = useState('');
+  const [mariah, setMariah] = useState('');
 
-
-  changeHandler = (e) => {
-    this.setState({[e.target.name] : e.target.value})
-
-  }
-
-  submitHandler = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
 
-    axios.post('https://sheet.best/api/sheets/81059974-ba71-4082-9824-74856dda88be', this.state)
-    .then(response => {
+    const objt = { beyonce, whitney, britney, mariah };
+      
+    axios
+      .post(
+        'https://sheet.best/api/sheets/81059974-ba71-4082-9824-74856dda88be', 
+        objt
+      )
+    .then((response) => {
       console.log(response);
-    })
-  }
+    });
+  };
   
-  render() {
-    const { beyonce, whitney, britney, mariah } = this.state;
-    return (
-      <div className="wrapper">
-        <div className="top">
-          <Header as='h2'>let's go, girls</Header>
-        </div>
-        <Form className="form" onSubmit={this.submitHandler}>
-          <Form.Field>
-            <label>Favorite Beyonce song?</label>
-            <input 
-              placeholder='Enter song' 
-              type="text" 
-              name = "beyonce" 
-              value = {beyonce} 
-              onChange={this.changeHandler}
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Favorite Whitney song?</label>
-            <input 
-              placeholder='Enter song' 
-              type="text" 
-              name = "whitney" 
-              value = {whitney} 
-              onChange={this.changeHandler}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Favorite Britney song?</label>
-            <input 
-              placeholder='Enter song' 
-              type="text" 
-              name = "britney" 
-              value = {britney} 
-              onChange={this.changeHandler}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Favorite Mariah song?</label>
-            <input 
-              placeholder='Enter song' 
-              type="text" 
-              name = "mariah" 
-              value = {mariah} 
-              onChange={this.changeHandler}/>
-          </Form.Field>
-
-            <Button type='submit'>Submit</Button>
-          </Form>
+  return (
+    <div className="wrapper">
+      <div className="top">
+        <Header as='h2'>let's go, girls</Header>
       </div>
-    );
-  }
+      <Form className="form">
+        <Form.Field>
+          <label>Favorite Beyonce song?</label>
+          <input 
+            placeholder='Enter song' 
+            onChange={(e) => setBeyonce(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Favorite Whitney song?</label>
+          <input 
+            placeholder='Enter song' 
+            onChange={(e) => setWhitney(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Favorite Britney song?</label>
+          <input 
+            placeholder='Enter song' 
+            onChange={(e) => setBritney(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Favorite Mariah song?</label>
+          <input 
+            placeholder='Enter song' 
+            onChange={(e) => setMariah(e.target.value)}
+          />
+        </Form.Field>
+
+        <Button type='submit' onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
+
+export default App;
